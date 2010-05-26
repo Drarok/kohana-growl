449e56bb584f69f0a71d08d2807088bfbfacfff1
Mat Gadd <mat.gadd@advanceitgroup.co.uk>
2010-05-26 17:16:18 +0100

    Track which hosts are registered. Remove the ability to register manually.

--- helpers/growl.php
+++ helpers/growl.php
@@ -4,29 +4,52 @@
 require_once(Kohana::find_file('vendor', 'Growl', TRUE));
 
 abstract class growl_Core {
-	// Cache the connections.
+	// Remember where we have registered.
+	protected static $registered = array();
+
+	// Connection cache.
 	protected static $sockets = array();
 
-	public static function register($host_alias = 'default') {
+	protected static function register($host_alias = 'default') {
+		// Do not re-register.
+		if (growl::registered($host_alias)) {
+			return;
+		}
+
+		// Instantiate a new registration packet with config params.
 		$p = new GrowlRegistrationPacket(
 			Kohana::config('growl.app_name'),
 			Kohana::config('growl.hosts.'.$host_alias.'.password')
 		);
 
+		// Add the notifications to the packet.
 		foreach (Kohana::config('growl.notifications') as $name => $enabled) {
 			$p->addNotification($name, $enabled);
 		}
 
+		// Transmit the packet.
 		growl::send($host_alias, $p->payload());
+
+		// Log the fact that we have registered.
+		growl::$registered[$host_alias] = TRUE;
+	}
+
+	protected static function registered($host_alias = 'default') {
+		return array_key_exists($host_alias, growl::$registered);
 	}
 
 	public static function notify($name, $title, $description, $host_alias = 'default') {
+		// Ensure we are registered first.
+		growl::registered($host_alias) OR growl::register($host_alias);
+
+		// Instantiate a new notification packet with the relevant options.
 		$p = new GrowlNotificationPacket(
 			Kohana::config('growl.app_name'),
 			$name, $title, $description, -2, TRUE,
 			Kohana::config('growl.hosts.'.$host_alias.'.password')
 		);
 
+		// Transmit the packet.
 		growl::send($host_alias, $p->payload());
 	}
 
